5a7637489852449d7bf438a0000a2ab30dd74662
Ismail Abdifadil <ismail.haabare@gmail.com>
2024-10-06 20:31:01 +0300

    Added evnironment variables

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBjYgJNx0rPUZs2yHeK4lZivDEoUA74q2U",
-  authDomain: "ntf-clone-e5809.firebaseapp.com",
-  projectId: "ntf-clone-e5809",
-  storageBucket: "ntf-clone-e5809.appspot.com",
-  messagingSenderId: "374267356568",
-  appId: "1:374267356568:web:c081224b53cdf45f40f7ed",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
